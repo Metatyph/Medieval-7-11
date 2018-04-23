cdbadc4a06083d04c6bbb439c0b71ec22bccd194
Metatyph <icun.prayitno@gmail.com>
2018-04-23 13:59:49 +1000

    Docket interaction

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager> {
+
+	private bool _isDocket = false;
+
+	public bool IsDocket {
+		get { return _isDocket; }
+		set { _isDocket = value; }
+	}
+
+
+}
